75d977d9b98a2627d4e603ec53830c32dff80989
marcosplural <marcosplural@gmail.com>
2025-07-16 15:21:12 -0300

    feat: servicos da api de servicos e config da pipeline

--- src/main/java/br/com/cotiinformatica/controllers/PedidosController.java
+++ src/main/java/br/com/cotiinformatica/controllers/PedidosController.java
@@ -1,49 +1,95 @@
 package br.com.cotiinformatica.controllers;
 
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.DeleteExchange;
-
+import br.com.cotiinformatica.domain.models.PedidoRequestModel;
+import br.com.cotiinformatica.domain.models.PedidoResponseModel;
+import br.com.cotiinformatica.domain.services.interfaces.PedidoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/pedidos")
-@Tag (name = "Controle de pediods" , description = "Serviços para gerenciamento de solicitações de pedidos.")
+@RequiredArgsConstructor
+@Tag(
+	name = "Controle de pedidos.",
+	description = "Serviços para gerenciamento de solicitiações de pedidos."
+)
 public class PedidosController {
+	private final PedidoService pedidoService;
 	
-	@Operation (summary = "Cadastro de solicitações de pedido.", description = "Cria uma nova solitação de pedido no sistema.")
+	@Operation(
+		summary = "Cadastro de solicitações de pedido.",
+		description = "Cria uma nova solicitação de pedido no sistema."
+	)
 	@PostMapping
-	public ResponseEntity<?> post() {
-		//TODO cadastro do pedido
-		return ResponseEntity.ok().build();
+	public ResponseEntity<PedidoResponseModel> post(@RequestBody @Valid PedidoRequestModel model) {
+		var response = pedidoService.criarPedido(model);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 	
-	@Operation (summary = "Atualização de pedido.", description = "Atualiza uma solitação de pedido no sistema.")
-	@PutMapping
-	public ResponseEntity<?> put() {
-		//TODO atualizacao do pedido
-		return ResponseEntity.ok().build();
+	@Operation(
+		summary = "Atualização de pedido.",
+		description = "Modifica uma solicitação de pedido existente no sistema."
+	)
+	@PutMapping("{id}")
+	public ResponseEntity<PedidoResponseModel> put(@PathVariable UUID id, @RequestBody @Valid PedidoRequestModel model) {
+		var response = pedidoService.alterarPedido(id, model);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
-	@Operation (summary = "Inativação de solicitações de pedido.", description = "Inativa uma solitação de pedido no sistema.")
-	@DeleteExchange
-	public ResponseEntity<?> delete() {
-		//TODO exclusao do pedido
-		return ResponseEntity.ok().build();
+	@Operation(
+		summary = "Inativação de solicitações de pedido.",
+		description = "Inativa uma solicitação de pedido existente no sistema."
+	)
+	@DeleteMapping("{id}")
+	public ResponseEntity<PedidoResponseModel> delete(@PathVariable UUID id) {
+		var response = pedidoService.inativarPedido(id);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
-	@Operation (summary = "Consulta de solicitações de pedido.", description = "Retorna uma solitação de pedido no sistema.")
+	@Operation(
+		summary = "Consulta de solicitações de pedido.",
+		description = "Retorna uma consulta paginada de solicitações de pedidos no sistema."
+	)
 	@GetMapping
-	public ResponseEntity<?> get() {
-		//TODO consulta do pedido
-		return ResponseEntity.ok().build();
+	public ResponseEntity<Page<PedidoResponseModel>> getAll(
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "25") int size
+			) {
+		
+		var pageable = PageRequest.of(page, size);
+		var response = pedidoService.consultarPedidos(pageable);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
-	
-
+	@Operation(
+			summary = "Consulta de solicitação de pedido por ID.",
+			description = "Retorna um pedido no sistema através do ID informado."
+		)
+	@GetMapping("{id}")
+	public ResponseEntity<PedidoResponseModel> getById(@PathVariable UUID id) {
+		
+		var response = pedidoService.obterPedidoPorId(id);
+			
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
 }
+
+
+
---
+++ src/main/java/br/com/cotiinformatica/domain/exceptions/PedidoNaoEncontradoException.java
@@ -0,0 +1,19 @@
+package br.com.cotiinformatica.domain.exceptions;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class PedidoNaoEncontradoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	private final UUID pedidoId;
+	
+	@Override
+	public String getMessage() {	
+		return MessageFormat.format("O pedido ''{0}'' não foi encontrado", pedidoId);
+	}	
+}
+
+
+
